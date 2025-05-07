7c128a7f0a6fb3ddb323ca2f4eb3ceaa98d9d228
Muhammad Ahmad <ahmad.amir@dubizzlelabs.com>
2025-05-07 09:25:08 +0500

    Added initial project skeleton

---
+++ main.py
@@ -0,0 +1,20 @@
+from app.config.config import settings
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.host,
+        port=settings.port,
+        reload=True,
+    )
