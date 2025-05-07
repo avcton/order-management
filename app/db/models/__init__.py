886fb83bc90a35e88a8fbdfa32ec14c5ad963757
Muhammad Ahmad <ahmad.amir@dubizzlelabs.com>
2025-05-07 14:01:58 +0500

    Added package initializer for models

---
+++ app/db/models/__init__.py
@@ -0,0 +1,7 @@
+from .user import User
+from .role import Role
+from .order import Order
+from .privilege import Privilege
+from .role_privilege import RolePrivilege
+
+__all__ = ["User", "Role", "Order", "Privilege", "RolePrivilege"]
