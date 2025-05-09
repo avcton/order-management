da7604ce7633cb9a62172d30c90ac90cadfd3f4d
Muhammad Ahmad <ahmad.amir@dubizzlelabs.com>
2025-05-09 11:57:39 +0500

    Added database session context manager

--- app/config/database.py
+++ app/config/database.py
@@ -9,3 +9,16 @@ engine = create_engine(DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+async def get_db_session():
+    """Simplest possible session context manager"""
+    session = SessionLocal()
+    try:
+        yield session
+        await session.commit()
+    except Exception:
+        await session.rollback()
+        raise
+    finally:
+        await session.close()
